748a70807aba0ae9bbd670338f0786e072d3a594
nirmal <nirmalspecial@gmail.com>
2019-12-31 00:13:19 +0530

    TestAppCheck

--- TestWeb/src/test/java/WebsiteTest/TestWeb/EnvSetup.java
+++ TestWeb/src/test/java/WebsiteTest/TestWeb/EnvSetup.java
@@ -1,5 +1,7 @@
 package WebsiteTest.TestWeb;
 
+import java.io.File;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -8,11 +10,13 @@ public class EnvSetup {
 	
 	public static void initDriver()
 	{
-		System.setProperty("webdriver.chrome.driver", "/home/ubuntu/jenkins/workspace/Test/TestWeb/src/test/java/Driver/chromedriver");
+		File file = new File("/home/ubuntu/jenkins/workspace/Test/TestWeb/src/test/java/Driver/chromedriver");
+		System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("disable-infobars");
 		options.addArguments("--disable-gpu");
 		options.addArguments("--headless");
+		options.setBinary("/home/ubuntu/jenkins/workspace/Test/TestWeb/src/test/java/Driver/chromedriver");
 		WebDriver driver = new ChromeDriver(options);
 		RunDriver.setDriver(driver);
 	}
