ba53dd48a249cd582e5ddb1bd677c63c805d58f7
nirmal <nirmalspecial@gmail.com>
2019-12-31 00:38:38 +0530

    TestAppCheck4

--- TestWeb/src/test/java/WebsiteTest/TestWeb/EnvSetup.java
+++ TestWeb/src/test/java/WebsiteTest/TestWeb/EnvSetup.java
@@ -17,6 +17,7 @@ public class EnvSetup {
 		options.addArguments("--disable-gpu");
 		options.addArguments("--no-sandbox");
 		options.addArguments("--disable-dev-shm-usage");
+		options.setExperimentalOption("useAutomationExtension",false);
 		options.addArguments("--headless");
 		options.setBinary("/home/ubuntu/jenkins/workspace/Test/TestWeb/src/test/java/Driver/chromedriver");
 		WebDriver driver = new ChromeDriver(options);
